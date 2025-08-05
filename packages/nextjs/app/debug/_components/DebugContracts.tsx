677d6a6c127ee2cc230f5bc548d06b09536a6dee
meetpaladiya44 <meetpaladiya2178@gmail.com>
2025-08-05 14:48:55 +0530

    completed multi-sig challenge with deployment on local nitro devnode

--- packages/nextjs/app/debug/_components/DebugContracts.tsx
+++ packages/nextjs/app/debug/_components/DebugContracts.tsx
@@ -32,7 +32,7 @@ export default function DebugContracts() {
     try {
       if (typeof window === "undefined") return;
 
-      const contractAddress = "0x85bd1a76e588e2539fd450afd6255806e7026049";
+      const contractAddress = "0xa6e41ffd769491a42a6e5ce453259b93983a22ef";
       const rpcUrl = process.env.NEXT_PUBLIC_RPC_URL || '';
       const privateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY || '';
       // const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
